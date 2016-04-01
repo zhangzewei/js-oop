9b7b3ef03d113dd71b22ba5928dce9da0ac04fae
zhangzewei <zhangzewei@testbird.com>
2016-04-01 17:12:03 +0800

    写了一个验证（初级的）

--- js-oop.js
+++ js-oop.js
@@ -1,10 +1,37 @@
+var things = ['asd','qwe','zxc','aaa'];
 var person = function(){
+	this.sayHello = function(name){
+		console.log('hello '+name);
+	};
+	this.doSomethings = function(things){
+		console.log('I can do '+things);
+	};
+};
+
+var student = function(){
+	this.goToSchool = function(name){
+		console.log(name+' go to school')
+	};
+};
+
+student.prototype = new person();
+var student = new student();
+
+var getPerson = function(){
 	return {
-		sayHello : function(name){
-			console.log('hello, ' + name);
-		},
-		doSomethings : function(things){
-			console.log('I can do'+ things)
+		init : function (){
+			var inputs = document.getElementsByClassName('test');
+			var inputsLength = document.getElementsByClassName('test').length;
+			for(let i = 0; i < inputsLength; i ++){
+				if(inputs[i].value == ''){
+					var b = '没有完成填写';
+					inputs[i].className += ' error-input';
+				}else{
+					inputs[i].className = 'test';
+				}
+			}
+			student.sayHello(b);
+			//student.doSomethings(thing);
 		},
 	};
 }();
